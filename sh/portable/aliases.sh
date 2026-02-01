a8978f147f3b312ff9dec4504d58adf91abbf117
patch-wreckless <234780031+patch-wreckless@users.noreply.github.com>
2026-02-01 14:40:28 -0400

    chore: init

---
+++ sh/portable/aliases.sh
@@ -0,0 +1,2 @@
+alias cargo-install='RUSTFLAGS="-C target-cpu=native -C opt-level=3" \
+	cargo install --locked --force'
---
+++ zsh/rc.zsh
@@ -0,0 +1,3 @@
+source "${0:a:h}/../sh/portable/aliases.sh"
+
+setopt INTERACTIVE_COMMENTS
