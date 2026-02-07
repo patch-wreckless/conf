11c5fa19deff34a11897231d3fb3f1de669ab782
patch-wreckless <234780031+patch-wreckless@users.noreply.github.com>
2026-02-07 11:38:01 -0400

    chore: configure csrc if installed

--- sh/portable/aliases.sh
+++ sh/portable/aliases.sh
@@ -4,6 +4,14 @@ alias cargo-install='RUSTFLAGS="-C target-cpu=native -C opt-level=3" \
 alias pb=pbcopy
 alias pbp=pbpaste
 
+if command -v csrc > /dev/null; then
+	csrc() {
+  		local dir
+		dir="$(command csrc "$@")" || return
+		[ -n "${dir}" ] && cd "${dir}"
+	}
+fi
+
 if command -v nvim > /dev/null; then
 	alias vi=nvim
 fi
