13848400a82cabe7ae3599d0d4ac7a64ad5cf3e3
patch-wreckless <234780031+patch-wreckless@users.noreply.github.com>
2026-02-07 16:33:32 -0400

    chore: render whitespace in nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,14 @@
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
+
+vim.opt.list = true
+vim.opt.listchars = {
+  multispace = "·",
+  nbsp = "+",
+  tab = "> ",
+  trail = "~",
+}
+
 vim.opt.number = true
 vim.opt.tabstop = 4
 
