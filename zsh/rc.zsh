42eadbde5dedd26d42e719c952846d1717fa45db
patch-wreckless <234780031+patch-wreckless@users.noreply.github.com>
2026-02-07 13:27:04 -0400

    chore: manage keybinds when EDITOR is set

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -1,4 +1,11 @@
 source "${0:a:h}/../sh/portable/aliases.sh"
 
+alias bke="bindkey -e"
+alias bkv="bindkey -v"
+
+# If EDITOR is set to something containing "vi" then zle will use vim-like
+# keybindings. Ensure it uses the defaults regardless of EDITOR.
+bke
+
 setopt INTERACTIVE_COMMENTS
 
