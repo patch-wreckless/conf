ae36a621c8f885afd1863206971f2e80e9be4402
patch-wreckless <234780031+patch-wreckless@users.noreply.github.com>
2026-04-14 21:20:22 -0300

    chore: use edit-command-line over key bind change
    Configures ^x^e to open the command line in nvim. This provides a better
    editing experience than vi mode keybindings and makes the bindkey
    aliases unnecessary.

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -2,12 +2,13 @@ CONF_ROOT="$(dirname $0)/.."
 
 . "${CONF_ROOT}/sh/portable/rc.sh"
 
-alias bke="bindkey -e"
-alias bkv="bindkey -v"
-
 # If EDITOR is set to something containing "vi" then zle will use vim-like
 # keybindings. Ensure it uses the defaults regardless of EDITOR.
 bindkey -e
+
+autoload -U edit-command-line
+zle -N edit-command-line
+bindkey '^x^e' edit-command-line
 
 setopt INTERACTIVE_COMMENTS
 
